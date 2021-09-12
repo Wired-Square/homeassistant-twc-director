4148865b68977e83fd438cbe333dc1edf5dec8f1
Garth Berry <garth@wiredsquare.com>
2021-09-12 07:35:55 +0100

    Add device state_class attribute for statistics

--- sensor.py
+++ sensor.py
@@ -4,7 +4,14 @@ import logging
 from homeassistant.const import (
     CONF_DEVICE_CLASS,
     CONF_FRIENDLY_NAME,
-    CONF_UNIT_OF_MEASUREMENT
+    CONF_UNIT_OF_MEASUREMENT,
+    DEVICE_CLASS_ENERGY,
+    ENERGY_KILO_WATT_HOUR,
+)
+
+from homeassistant.components.sensor import (
+    STATE_CLASS_MEASUREMENT,
+    STATE_CLASS_TOTAL_INCREASING,
 )
 
 from twcdirector.device import TWCPeripheral
@@ -27,9 +34,10 @@ _LOGGER = logging.getLogger(__name__)
 SENSOR_TYPES = {
     "total_kwh": {
         CONF_FRIENDLY_NAME: "Total Energy Delivered",
-        CONF_DEVICE_CLASS: "energy",
+        CONF_DEVICE_CLASS: DEVICE_CLASS_ENERGY,
         CONF_SCALE: 1,
-        CONF_UNIT_OF_MEASUREMENT: "kWh"
+        CONF_UNIT_OF_MEASUREMENT: ENERGY_KILO_WATT_HOUR,
+        "state_class": STATE_CLASS_TOTAL_INCREASING
     },
     "voltage_phase_l1": {
         CONF_FRIENDLY_NAME: "AC Voltage Phase 1",
@@ -155,6 +163,7 @@ class TWCStateSensor(TWCDeviceEntity):
         self._entity_attribute = entity_attribute
         self._entity_detail = entity_detail
         self._unit_of_measure = entity_detail[CONF_UNIT_OF_MEASUREMENT]
+        self._state_class = entity_detail.get("state_class", None)
         self._round = entity_detail.get(CONF_ROUND, None)
         self._format = entity_detail.get(CONF_FORMAT, None)
         self._scale = entity_detail.get(CONF_SCALE, None)
@@ -181,6 +190,10 @@ class TWCStateSensor(TWCDeviceEntity):
     @property
     def device_class(self):
         return self._device_class
+
+    @property
+    def state_class(self):
+        return self._state_class
 
     @property
     def device_state_attributes(self):
